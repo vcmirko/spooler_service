ca2b24c5e7be917eaa8bfb97d6b2f4d22185fefe
Pascal Scheiben <flohzirkus666@gmail.com>
2025-06-02 07:39:14 +0200

    feat: replacing if/else in secret_factory.py

--- flow_processor/secret_factory.py
+++ flow_processor/secret_factory.py
@@ -9,16 +9,16 @@ from .secrets.token_secret import TokenSecret
 class SecretFactory:
     @staticmethod
     def load(secret_def):
-        secret = {}
         secret_type = secret_def.get("type", "credential")
-        if secret_type == "credential":
-            secret = CredentialSecret(secret_def).load()
-        elif secret_type == "token":
-            secret = TokenSecret(secret_def).load()
-        elif secret_type == "api-key":
-            secret = ApiKeySecret(secret_def).load()
-        elif secret_type == "hashicorp-vault":
-            secret = HashicorpVaultSecret(secret_def).load()
-        else:
-            raise Exception(f"Unknown secret type: {secret_type}")
+        match secret_type:
+            case "credential":
+                secret = CredentialSecret(secret_def).load()
+            case "token":
+                secret = TokenSecret(secret_def).load()
+            case "api-key":
+                secret = ApiKeySecret(secret_def).load()
+            case "hashicorp-vault":
+                secret = HashicorpVaultSecret(secret_def).load()
+            case _:
+                raise Exception(f"Unknown secret type: {secret_type}")
         return secret
