19fed3b20d5db89a867b788d8b567e73bd9ca8ce
vcmirko <mirko@vancolen.com>
2025-06-18 19:18:29 +0200

    fix

--- flow_processor/steps/goto_step.py
+++ flow_processor/steps/goto_step.py
@@ -8,13 +8,15 @@ class GotoStep(Step):
     def __init__(self, step, flow):
         super().__init__(step, flow)
         assert "goto" in step, "Goto configuration is required"
-        self.step_name = apply_jinja2(step.get("goto"), self._flow._data)
-        assert isinstance(self.step_name, str), "Goto step name must be a string"
-        assert self.step_name, "Goto step name cannot be empty"
+        self._goto = step.get("goto")
+        self._step_name = self._goto.get("step_name")
+        assert isinstance(self._step_name, str), "Goto step name must be a string"
+        assert self._step_name, "Goto step name cannot be empty"
+        self._step_name = apply_jinja2(self._step_name, self._flow._data)
 
     def process(self):
         # Just return the step name to jump to
-        return {"goto": self.step_name}
+        return {"goto": self._step_name}
 
     def process(self, ignore_when=False):
         """Process the goto step."""
@@ -24,7 +26,7 @@ class GotoStep(Step):
         if not enabled:
             return
 
-        logging.info("%s -> %s", self._representation, self._expression)
-        return {"goto": self.step_name}
+        logging.info("%s -> %s", self._representation, self._step_name)
+        return {"goto": self._step_name}
     
         # no post processing needed for goto step, it just returns the step name to jump to
